d25aab7ea880e1293c3ff1dc3b40ef17ffb0f4ed
Nazareth Gomez <nazarethgomez@MacBook-Pro-de-Nazareth.local>
2023-11-15 19:17:02 -0600

    nuevo commit

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,4 +1,19 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+    @IsOptional()
+    @IsString()
+    url: string;
+
+    @IsOptional()
+    @IsNumber()
+    id: number;
+
+
+    @IsOptional()
+    @IsOptional()
+    Description: string;
+
+}
