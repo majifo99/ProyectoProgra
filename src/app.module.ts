3f28902ae7b6a8559fa6e095e19f2a9486c4d861
majifo99 <135087859+majifo99@users.noreply.github.com>
2023-11-15 21:29:22 -0600

    actualizacion

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from './companys/companys.module';
-import { ServicesModule } from './services/services.module';
+import { ServiceModule } from './services/services.module';
 import { GalerysModule } from './galerys/galerys.module';
 import { Galery } from './galerys/entities/galery.entity';
 import { Company } from './companys/entities/company.entity';
@@ -11,7 +11,7 @@ import { Service } from './services/entities/service.entity';
 @Module({
   imports: [
  
-    ServicesModule,
+    ServiceModule,
     CompanyModule,
     GalerysModule,
    
@@ -20,19 +20,15 @@ import { Service } from './services/entities/service.entity';
       host: 'localhost',
       port: 3306,
       username: 'root',
-<<<<<<< HEAD
       password:'123456',
       database:'api',
-=======
-      password:'admin123',
-      database:'project',
->>>>>>> 913297deb79c7c7060c6d117db5fc66dad6255bd
+
       entities: [Company,Service,Galery],
       autoLoadEntities: true,
       synchronize: true
     }),
     CompanyModule,
-    ServicesModule,
+    ServiceModule,
     GalerysModule,
 
   ],
