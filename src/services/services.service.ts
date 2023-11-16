a8979117cc90cdc96cbc55b6357fa219fdcbfe9c
Naza Gomez <nazasanchez12.26@gmail.com>
2023-11-15 21:13:43 -0600

    new changes

--- src/services/services.service.ts
+++ src/services/services.service.ts
@@ -1,26 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
+import { Service } from './entities/service.entity';
+import { Repository } from 'typeorm';
+import { throws } from 'assert';
 
 @Injectable()
 export class ServicesService {
-  create(createServiceDto: CreateServiceDto) {
-    return 'This action adds a new service';
+  constructor(
+    @InjectRepository(Service)
+    private ServiceRepository: Repository<Service>,
+    ) {  }
+
+  async create(createServiceDto: CreateServiceDto) {
+    const company = await this.ServiceRepository.findOneBy({ id: createServiceDto.id});
+    if (!company) {
+    
+      throw new BadRequestException('company not found');
+    }
+
+    const service = this.ServiceRepository.create({
+    ...createServiceDto, company})
+
+    return  await this.ServiceRepository.save(service);
+    }
+
+  async findAll() {
+    return await  this.ServiceRepository.find();
   }
 
-  findAll() {
-    return `This action returns all services`;
+  async findOne(id: number) {
+    const service = await this.ServiceRepository.findOne({ where: { id } });
+    if (!service) {
+    throw new BadRequestException( 'Service not found');
+    }
+    return service;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  async update(id: number, updateServiceDto: UpdateServiceDto) {
+    const service = await this.ServiceRepository.findOne({ where: { id: id } });
+    if (!service) {
+      throw new BadRequestException(' Service not found');
+    }
+
+    let company;
+    if (updateServiceDto.id) {
+      company = await this.ServiceRepository.findOneBy({ id: updateServiceDto.id });
+    if (!company) {
+      throw new BadRequestException('company not found');
+    }
   }
 
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
+  const updatedService = this.ServiceRepository.merge(service, {
+  ...updateServiceDto,
+  company
+  });
+return await this.ServiceRepository.save(updatedService);
+}
+
+
+  async remove(id: number) {
+    const existingService = await this.ServiceRepository.findOne({ where: { id } });
+    if (!existingService) {
+    throw new BadRequestException( ' Service not found');
+    }
+    return await this.ServiceRepository.remove(existingService);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} service`;
-  }
+
 }
