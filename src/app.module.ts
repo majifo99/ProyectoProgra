b31fd2d34e68360c4e647b15e2e482327908b853
Naza Gomez <nazasanchez12.26@gmail.com>
2023-11-15 21:25:54 -0600

    chaanges naza

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from './companys/companys.module';
-import { ServicesModule } from './services/services.module';
+import { ServiceModule } from './services/services.module';
 import { GalerysModule } from './galerys/galerys.module';
 import { Galery } from './galerys/entities/galery.entity';
 import { Company } from './companys/entities/company.entity';
@@ -11,7 +11,7 @@ import { Service } from './services/entities/service.entity';
 @Module({
   imports: [
  
-    ServicesModule,
+    ServiceModule,
     CompanyModule,
     GalerysModule,
    
@@ -27,7 +27,7 @@ import { Service } from './services/entities/service.entity';
       synchronize: true
     }),
     CompanyModule,
-    ServicesModule,
+    ServiceModule,
     GalerysModule,
 
   ],
